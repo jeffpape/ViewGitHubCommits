1cfda5bd290228a8f8f4fd2333081f55c239e38c
Jeff Pape <jeff.pape@gmail.com>
2019-11-03 19:19:17 -0600

    Initial Commit

---
+++ ViewGitHubCommits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewGitHubCommits
+//
+//  Created by Jeff Pape on 11/3/19.
+//  Copyright © 2019 Pape Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
